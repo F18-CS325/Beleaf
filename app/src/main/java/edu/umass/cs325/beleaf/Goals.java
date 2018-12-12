6c409c6099946b87ce993fd23c4174461f4fa055
Mina <minaalexandra96@gmail.com>
2018-12-12 10:51:24 -0500

    activity_goals

---
+++ app/src/main/java/edu/umass/cs325/beleaf/Goals.java
@@ -0,0 +1,52 @@
+package edu.umass.cs325.beleaf;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import edu.umass.cs325.beleaf.R;
+
+public class Goals extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_news:
+                    mTextMessage.setText(R.string.title_news);
+                    return true;
+                case R.id.navigation_footprint:
+                    mTextMessage.setText(R.string.title_footprint);
+                    return true;
+                case R.id.navigation_forest:
+                    mTextMessage.setText(R.string.title_forest);
+                    return true;
+                case R.id.navigation_goals:
+                    mTextMessage.setText(R.string.title_goals);
+                    return true;
+                case R.id.navigation_me:
+                    mTextMessage.setText(R.string.title_me);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
