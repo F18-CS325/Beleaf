c9c52e36fa47fb1ce143c032a6aa385a7165e154
Mina <minaalexandra96@gmail.com>
2018-12-14 17:39:19 -0500

    add more footprint stuff

--- app/src/main/java/edu/umass/cs325/beleaf/Footprint3.java
+++ app/src/main/java/edu/umass/cs325/beleaf/Footprint3.java
@@ -1,10 +1,13 @@
 package edu.umass.cs325.beleaf;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Footprint3 extends AppCompatActivity {
@@ -45,6 +48,15 @@ public class Footprint3 extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent=new Intent(Footprint3.this,Footprint4.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/edu/umass/cs325/beleaf/Footprint4.java
+++ app/src/main/java/edu/umass/cs325/beleaf/Footprint4.java
@@ -1,18 +1,13 @@
 package edu.umass.cs325.beleaf;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Footprint2 extends AppCompatActivity {
+public class Footprint4 extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -45,38 +40,11 @@ public class Footprint2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_footprint2);
+        setContentView(R.layout.activity_footprint4);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                Intent intent=new Intent(Footprint2.this,Footprint3.class);
-                startActivity(intent);
-            }
-        });
-
-        Button skipbutton = (Button) findViewById(R.id.skipbutton);
-        skipbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                Intent intent=new Intent(Footprint2.this,Footprint4.class);
-                startActivity(intent);
-            }
-        });
-
-        final ImageView carButton = (ImageView) findViewById(R.id.carbutton);
-        carButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                carButton.setPressed(true);
-                return true;
-            }
-        });
     }
 
 }
